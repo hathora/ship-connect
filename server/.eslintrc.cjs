0ee5bd01f8fb5812f132f54c364e17ab612c3488
Harsh Pandey <hpandey793@gmail.com>
2022-06-12 14:34:10 -0400

    initial commit

---
+++ server/.eslintrc.cjs
@@ -0,0 +1,33 @@
+module.exports = {
+  env: {
+    node: true,
+  },
+  extends: ["eslint:recommended", "plugin:@typescript-eslint/recommended"],
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module",
+  },
+  plugins: ["@typescript-eslint", "eslint-plugin-import", "prettier"],
+  rules: {
+    "prettier/prettier": ["error", { printWidth: 120 }],
+    indent: ["error", 2],
+    "linebreak-style": ["error", "unix"],
+    quotes: ["error", "double"],
+    semi: ["error", "always"],
+    "import/order": [
+      "error",
+      {
+        groups: ["builtin", "external", "internal", "parent", "sibling", "index", "object", "type"],
+        "newlines-between": "always",
+        alphabetize: {
+          order: "desc",
+          caseInsensitive: true,
+        },
+      },
+    ],
+    curly: ["error"],
+    "arrow-parens": ["error"],
+    "arrow-spacing": ["error"],
+  },
+};
---
+++ server/impl.ts
@@ -0,0 +1,78 @@
+import {
+  GameState,
+  UserId,
+  IUpdateRotationRequest,
+  IUpdateAcceleratingRequest,
+  PlayerShip,
+  Rotation,
+} from "../api/types";
+import { Response } from "../api/base";
+
+import { Methods, Context } from "./.hathora/methods";
+
+type InternalShip = PlayerShip & {
+  rotation: Rotation;
+  accelerating: boolean;
+};
+type InternalState = {
+  ships: InternalShip[];
+};
+
+const SHIP_ROTATION_SPEED = 0.01; // radians per second
+const SHIP_SPEED = 100; // pixels per second
+
+export class Impl implements Methods<InternalState> {
+  initialize(): InternalState {
+    return { ships: [] };
+  }
+  joinGame(state: InternalState, userId: UserId): Response {
+    if (state.ships.some((s) => s.player === userId)) {
+      return Response.error("Already joined");
+    }
+    state.ships.push({
+      player: userId,
+      location: { x: 0, y: 0 },
+      angle: 0,
+      rotation: Rotation.NONE,
+      accelerating: false,
+    });
+    return Response.ok();
+  }
+  updateRotation(state: InternalState, userId: UserId, ctx: Context, request: IUpdateRotationRequest): Response {
+    const ship = state.ships.find((s) => s.player === userId);
+    if (ship === undefined) {
+      return Response.error("Not joined");
+    }
+    ship.rotation = request.rotation;
+    return Response.ok();
+  }
+  updateAccelerating(
+    state: InternalState,
+    userId: UserId,
+    ctx: Context,
+    request: IUpdateAcceleratingRequest
+  ): Response {
+    const ship = state.ships.find((s) => s.player === userId);
+    if (ship === undefined) {
+      return Response.error("Not joined");
+    }
+    ship.accelerating = request.accelerating;
+    return Response.ok();
+  }
+  onTick(state: InternalState, ctx: Context, timeDelta: number): void {
+    state.ships.forEach((ship) => {
+      if (ship.rotation === Rotation.LEFT) {
+        ship.angle -= SHIP_ROTATION_SPEED;
+      } else if (ship.rotation === Rotation.RIGHT) {
+        ship.angle += SHIP_ROTATION_SPEED;
+      }
+      if (ship.accelerating) {
+        ship.location.x += Math.cos(ship.angle) * SHIP_SPEED * timeDelta;
+        ship.location.y += Math.sin(ship.angle) * SHIP_SPEED * timeDelta;
+      }
+    });
+  }
+  getUserState(state: InternalState): GameState {
+    return state;
+  }
+}
