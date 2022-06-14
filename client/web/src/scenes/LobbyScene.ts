226df4a09fcbbece6191f283f7e7c93ccf986c7b
Harsh Pandey <hpandey793@gmail.com>
2022-06-14 12:59:03 -0400

    allow joining existing room

--- client/web/src/scenes/LobbyScene.ts
+++ client/web/src/scenes/LobbyScene.ts
@@ -1,3 +1,5 @@
+import InputText from "phaser3-rex-plugins/plugins/inputtext";
+
 import { HathoraClient } from "../../../.hathora/client";
 import { GAME_HEIGHT, GAME_WIDTH } from "../consts";
 
@@ -7,6 +9,8 @@ export class LobbyScene extends Phaser.Scene {
   }
 
   create() {
+    const client = new HathoraClient();
+
     const createButton = this.add
       .text(GAME_WIDTH / 2, GAME_HEIGHT / 4, "Create New Game", {
         fontSize: "20px",
@@ -20,7 +24,6 @@ export class LobbyScene extends Phaser.Scene {
       .on("pointerover", () => createButton.setStyle({ fill: "#f39c12" }))
       .on("pointerout", () => createButton.setStyle({ fill: "#FFF" }))
       .on("pointerdown", async () => {
-        const client = new HathoraClient();
         if (sessionStorage.getItem("token") === null) {
           const newToken = await client.loginAnonymous();
           sessionStorage.setItem("token", newToken);
@@ -31,5 +34,44 @@ export class LobbyScene extends Phaser.Scene {
         const connection = await client.connect(token, stateId);
         this.scene.start("game", { connection });
       });
+
+    const joinButton = this.add
+      .text(GAME_WIDTH / 2, (GAME_HEIGHT * 3) / 4, "Join Existing Game", {
+        fontSize: "20px",
+        fontFamily: "futura",
+      })
+      .setInteractive({ useHandCursor: true })
+      .setOrigin(0.5)
+      .setPadding(10)
+      .setStyle({ backgroundColor: "#111" })
+      .setInteractive({ useHandCursor: true })
+      .on("pointerover", () => joinButton.setStyle({ fill: "#f39c12" }))
+      .on("pointerout", () => joinButton.setStyle({ fill: "#FFF" }))
+      .on("pointerdown", async () => {
+        const stateId = inputText.text?.trim();
+        if (stateId === undefined || stateId === "") {
+          alert("Please enter an existing room code or create a new game!");
+          return;
+        }
+        if (sessionStorage.getItem("token") === null) {
+          const newToken = await client.loginAnonymous();
+          sessionStorage.setItem("token", newToken);
+        }
+        // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+        const token = sessionStorage.getItem("token")!;
+        const connection = await client.connect(token, stateId);
+        this.scene.start("game", { connection });
+      });
+
+    const inputTextConfig: InputText.IConfig = {
+      border: 10,
+      borderColor: "black",
+      backgroundColor: "white",
+      placeholder: "Room Code",
+      color: "black",
+      fontFamily: "futura",
+    };
+    const inputText = new InputText(this, joinButton.x, joinButton.y - 40, 100, 30, inputTextConfig);
+    this.add.existing(inputText);
   }
 }
